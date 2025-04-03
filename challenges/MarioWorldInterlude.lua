b5d5798f2ad0ffd8f7019708873092047c19ad14
Theo <Theomn@users.noreply.github.com>
2025-04-03 18:27:21 -0400

    Interlude timer freeze

--- challenges/MarioWorldInterlude.lua
+++ challenges/MarioWorldInterlude.lua
@@ -9,13 +9,14 @@ return {
         weight = 1,
         handler = function(state, reset)
             local text = "Are you comfortable?\nAre you having fun?"
-            gui.drawRectangle(24, 39, 206, 96, "black", "black")
+            gui.drawRectangle(24, 39, 206, 92, "black", "black")
             gui.drawText(128, 46, "CHECKPOINT!", "#FFFB00", "#8C5918", 16, nil, "bold", "center")
             gui.drawText(128, 66, "Talk to each other!", "#5AAAF7", nil, 14, nil, "bold", "center")
-            gui.drawText(128, 86, text, "white", nil, nil, nil, nil, "center")
-            gui.drawText(128, 113, "Continue?", "white", nil, 14, nil, "bold", "center")
+            gui.drawText(128, 82, text, "white", nil, nil, nil, nil, "center")
+            gui.drawText(128, 109, "Continue?", "white", nil, 14, nil, "bold", "center")
             local magic_number = mainmemory.read_u16_le(0x000DDA)
             local popup_id = mainmemory.read_u16_le(0x0013D2)
+            mainmemory.writebyte(0x000F30, 255)
 
             if popup_id == 1 then
                 return 0.8
